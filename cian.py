d27ec6e4f8b4254547061428c7d55d061d4add60
Векшин Никита <vekshinnikita@yandex.ru>
2025-08-02 19:16:48 +0300

    fix

--- cian.py
+++ cian.py
@@ -6,263 +6,293 @@ from base import BaseFinder, FinderABC
 from utils import get_date_from_string
 
 
-
 class CianFinder(BaseFinder):
-    
-    url = 'https://api.cian.ru/search-offers/v3/search-offers-desktop/'
+
+    url = 'https://api.cian.ru/search-offers/v2/search-offers-desktop/'
     offer_url = 'https://spb.cian.ru/rent/flat/{id}/'
     filter = {
         "jsonQuery": {
             "_type": "flatrent",
             "sort": {
-            "type": "term",
-            "value": "creation_date_desc"
+                "type": "term",
+                "value": "creation_date_desc"
             },
             "engine_version": {
-            "type": "term",
-            "value": 2
+                "type": "term",
+                "value": 2
             },
             "price": {
-            "type": "range",
-            "value": {
-                "gte": 25000,
-                "lte": 35000
-            }
+                "type": "range",
+                "value": {
+                    "gte": 25000,
+                    "lte": 35000
+                }
             },
             "currency": {
-            "type": "term",
-            "value": 2
+                "type": "term",
+                "value": 2
             },
             "geo": {
-            "type": "geo",
-            "value": [
-                {
-                "type": "polygon",
-                "name": "Область поиска",
-                "coordinates": [
-                    [
-                    "30.3163868",
-                    "60.0530472"
-                    ],
-                    [
-                    "30.2806812",
-                    "60.0489117"
-                    ],
-                    [
-                    "30.2504688",
-                    "60.0378838"
-                    ],
-                    [
-                    "30.2243763",
-                    "60.0254774"
-                    ],
-                    [
-                    "30.2037769",
-                    "60.0116924"
-                    ],
-                    [
-                    "30.1955372",
-                    "59.9951505"
-                    ],
-                    [
-                    "30.1872974",
-                    "59.9786087"
-                    ],
-                    [
-                    "30.1818043",
-                    "59.9593098"
-                    ],
-                    [
-                    "30.1859242",
-                    "59.9420786"
-                    ],
-                    [
-                    "30.2024036",
-                    "59.926226"
-                    ],
-                    [
-                    "30.2257496",
-                    "59.9145088"
-                    ],
-                    [
-                    "30.255962",
-                    "59.9048594"
-                    ],
-                    [
-                    "30.2724415",
-                    "59.8903852"
-                    ],
-                    [
-                    "30.2930409",
-                    "59.8772895"
-                    ],
-                    [
-                    "30.2944141",
-                    "59.8600584"
-                    ],
-                    [
-                    "30.288921",
-                    "59.8435165"
-                    ],
-                    [
-                    "30.2930409",
-                    "59.8269746"
-                    ],
-                    [
-                    "30.3122669",
-                    "59.8118112"
-                    ],
-                    [
-                    "30.3452259",
-                    "59.808365"
-                    ],
-                    [
-                    "30.3795582",
-                    "59.8076757"
-                    ],
-                    [
-                    "30.4111439",
-                    "59.8138789"
-                    ],
-                    [
-                    "30.439983",
-                    "59.8228391"
-                    ],
-                    [
-                    "30.4674488",
-                    "59.8331778"
-                    ],
-                    [
-                    "30.5004078",
-                    "59.8324886"
-                    ],
-                    [
-                    "30.5292469",
-                    "59.8242176"
-                    ],
-                    [
-                    "30.5622059",
-                    "59.8262854"
-                    ],
-                    [
-                    "30.5306202",
-                    "59.8324886"
-                    ],
-                    [
-                    "30.5100208",
-                    "59.8455842"
-                    ],
-                    [
-                    "30.505901",
-                    "59.8628154"
-                    ],
-                    [
-                    "30.4949146",
-                    "59.8793573"
-                    ],
-                    [
-                    "30.482555",
-                    "59.8952099"
-                    ],
-                    [
-                    "30.4784351",
-                    "59.9124411"
-                    ],
-                    [
-                    "30.4743153",
-                    "59.9296722"
-                    ],
-                    [
-                    "30.4756886",
-                    "59.9462141"
-                    ],
-                    [
-                    "30.4798084",
-                    "59.962756"
-                    ],
-                    [
-                    "30.4633289",
-                    "59.9792979"
-                    ],
-                    [
-                    "30.4468495",
-                    "59.9972183"
-                    ],
-                    [
-                    "30.4289967",
-                    "60.0116924"
-                    ],
-                    [
-                    "30.4193836",
-                    "60.0296128"
-                    ],
-                    [
-                    "30.3919178",
-                    "60.046844"
-                    ],
-                    [
-                    "30.3658253",
-                    "60.0571827"
-                    ],
-                    [
-                    "30.3328663",
-                    "60.0599396"
-                    ],
-                    [
-                    "30.2999073",
-                    "60.0551149"
-                    ],
-                    [
-                    "30.3163868",
-                    "60.0530472"
-                    ]
+                "type": "geo",
+                "value": [
+                    {
+                        "type": "underground",
+                        "id": 171
+                    },
+                    {
+                        "type": "underground",
+                        "id": 172
+                    },
+                    {
+                        "type": "underground",
+                        "id": 173
+                    },
+                    {
+                        "type": "underground",
+                        "id": 174
+                    },
+                    {
+                        "type": "underground",
+                        "id": 175
+                    },
+                    {
+                        "type": "underground",
+                        "id": 176
+                    },
+                    {
+                        "type": "underground",
+                        "id": 177
+                    },
+                    {
+                        "type": "underground",
+                        "id": 178
+                    },
+                    {
+                        "type": "underground",
+                        "id": 179
+                    },
+                    {
+                        "type": "underground",
+                        "id": 180
+                    },
+                    {
+                        "type": "underground",
+                        "id": 181
+                    },
+                    {
+                        "type": "underground",
+                        "id": 182
+                    },
+                    {
+                        "type": "underground",
+                        "id": 191
+                    },
+                    {
+                        "type": "underground",
+                        "id": 192
+                    },
+                    {
+                        "type": "underground",
+                        "id": 193
+                    },
+                    {
+                        "type": "underground",
+                        "id": 194
+                    },
+                    {
+                        "type": "underground",
+                        "id": 195
+                    },
+                    {
+                        "type": "underground",
+                        "id": 197
+                    },
+                    {
+                        "type": "underground",
+                        "id": 198
+                    },
+                    {
+                        "type": "underground",
+                        "id": 199
+                    },
+                    {
+                        "type": "underground",
+                        "id": 200
+                    },
+                    {
+                        "type": "underground",
+                        "id": 201
+                    },
+                    {
+                        "type": "underground",
+                        "id": 202
+                    },
+                    {
+                        "type": "underground",
+                        "id": 204
+                    },
+                    {
+                        "type": "underground",
+                        "id": 205
+                    },
+                    {
+                        "type": "underground",
+                        "id": 206
+                    },
+                    {
+                        "type": "underground",
+                        "id": 207
+                    },
+                    {
+                        "type": "underground",
+                        "id": 208
+                    },
+                    {
+                        "type": "underground",
+                        "id": 210
+                    },
+                    {
+                        "type": "underground",
+                        "id": 211
+                    },
+                    {
+                        "type": "underground",
+                        "id": 212
+                    },
+                    {
+                        "type": "underground",
+                        "id": 213
+                    },
+                    {
+                        "type": "underground",
+                        "id": 214
+                    },
+                    {
+                        "type": "underground",
+                        "id": 218
+                    },
+                    {
+                        "type": "underground",
+                        "id": 219
+                    },
+                    {
+                        "type": "underground",
+                        "id": 220
+                    },
+                    {
+                        "type": "underground",
+                        "id": 221
+                    },
+                    {
+                        "type": "underground",
+                        "id": 222
+                    },
+                    {
+                        "type": "underground",
+                        "id": 224
+                    },
+                    {
+                        "type": "underground",
+                        "id": 225
+                    },
+                    {
+                        "type": "underground",
+                        "id": 226
+                    },
+                    {
+                        "type": "underground",
+                        "id": 227
+                    },
+                    {
+                        "type": "underground",
+                        "id": 230
+                    },
+                    {
+                        "type": "underground",
+                        "id": 231
+                    },
+                    {
+                        "type": "underground",
+                        "id": 232
+                    },
+                    {
+                        "type": "underground",
+                        "id": 241
+                    },
+                    {
+                        "type": "underground",
+                        "id": 242
+                    },
+                    {
+                        "type": "underground",
+                        "id": 246
+                    },
+                    {
+                        "type": "underground",
+                        "id": 247
+                    },
+                    {
+                        "type": "underground",
+                        "id": 355
+                    },
+                    {
+                        "type": "underground",
+                        "id": 356
+                    },
+                    {
+                        "type": "underground",
+                        "id": 357
+                    },
+                    {
+                        "type": "underground",
+                        "id": 382
+                    }
                 ]
+            },
+            "floor": {
+                "type": "range",
+                "value": {
+                    "gte": 2
                 }
-            ]
             },
-            "bbox": {
-            "type": "term",
-            "value": [
-                [
-                30.1987187088,
-                59.7513631609
-                ],
-                [
-                30.7755009354,
-                59.8808716541
-                ]
-            ]
-            },
-            "floorn": {
-            "type": "range",
-            "value": {
-                "gte": 2
-            }
+            "publish_period": {
+                "type": "term",
+                "value": -2
             },
             "saved_search_id": {
-            "type": "term",
-            "value": 51596133
+                "type": "term",
+                "value": 51596133
             },
             "for_day": {
-            "type": "term",
-            "value": "!1"
+                "type": "term",
+                "value": "0"
             },
             "repair": {
-            "type": "terms",
-            "value": [
-                3,
-                4
-            ]
+                "type": "terms",
+                "value": [
+                    3,
+                    4
+                ]
             },
             "room": {
-            "type": "terms",
-            "value": [
-                1,
-                9
-            ]
+                "type": "terms",
+                "value": [
+                    1,
+                    9
+                ]
+            },
+            "foot_min": {
+                "type": "range",
+                "value": {
+                    "lte": 15
+                }
+            },
+            "only_foot": {
+                "type": "term",
+                "value": "2"
             }
         }
-        }
+    }
 
     def get_offers(self):
         print(f'Request Cian')
@@ -270,27 +300,26 @@ class CianFinder(BaseFinder):
             self.url,
             json=self.filter,
         )
-        
+
         try:
             return response.json().get('offers', [])
         except:
             print(response.text)
             return []
-    
+
     def get_offer_datetime(self, offer: Dict):
         datetime = get_date_from_string(offer['data']['updatedOn'])
         time_split = offer['data']['updatedAt'].split(',')[1].split(':')
 
         datetime = datetime.replace(
-            hour = int(time_split[0]),
-            minute = int(time_split[1])
+            hour=int(time_split[0]),
+            minute=int(time_split[1])
         )
         return datetime
-    
+
     def get_notification_message(self, offer: Dict):
         message = 'Появилось новое объявление на ЦИАН: {url}'
-        offer_id = offer['id']  
+        offer_id = offer['id']
         offer_url = self.offer_url.format(id=offer_id)
 
         return message.format(url=offer_url)
-    
